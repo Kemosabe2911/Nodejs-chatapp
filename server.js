b7d5eaaab4eed431f0485553f8ecd8bc98f24386
Kemosabe2911 <stevinprince29@gmail.com>
2020-05-01 21:02:13 +0530

    Server side broadcast

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const io= require('socket.io')(3000)
 
 io.on('connection', socket =>{
-    socket.emit('chat-message','Hello World')
+    socket.on('send-chat-message', message =>{
+        socket.broadcast.emit('chat-message', message)
+    })
 })

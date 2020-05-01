1ac2f9b07df79bb670b3317507438c1ee137f502
Kemosabe2911 <stevinprince29@gmail.com>
2020-05-01 21:16:18 +0530

    User joined message

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const messageContainer= document.getElementById('message-container')
 const messageForm= document.getElementById('send-container')
 const messageInput= document.getElementById('message-input')
 
+const name= prompt('What is your name?')
+appendMessage('You joined')
+socket.emit('new-user',name)
+
 socket.on('chat-message', data =>{
     appendMessage(data)
 })
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const io= require('socket.io')(3000)
+const users= {}
 
 io.on('connection', socket =>{
+    socket.on('new-user', name =>{
+        users[socket.id]= name
+        socket.broadcast.emit('user-connected', name)
+    })
     socket.on('send-chat-message', message =>{
         socket.broadcast.emit('chat-message', message)
     })

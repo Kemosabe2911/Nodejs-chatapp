6b824d2c2b7702e2c9441da49428ad48bc8588f7
Kemosabe2911 <stevinprince29@gmail.com>
2020-05-01 21:09:11 +0530

    Display message

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const socket= io('http://localhost:3000')
-
+const messageContainer= document.getElementById('message-container')
 const messageForm= document.getElementById('send-container')
 const messageInput= document.getElementById('message-input')
 
 socket.on('chat-message', data =>{
-    console.log(data)
+    appendMessage(data)
 })
 
 messageForm.addEventListener('submit', e=>{
@@ -13,3 +13,9 @@ messageForm.addEventListener('submit', e=>{
     socket.emit('send-chat-message',message)
     messageInput.value= ''
 })
+
+function appendMessage(message){
+    const messageElement= document.createElement('div')
+    messageElement.innerText= message
+    messageContainer.append(messageElement)
+}
